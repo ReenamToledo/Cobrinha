3122f7066ecc1fa0ea46fdfa90f7820c584ca75a
Reenam Toledo Fonseca <reenamtoledo364@gmail.com>
2025-07-08 16:50:51 -0300

    Add files via upload

--- vetor.py
+++ vetor.py
@@ -20,3 +20,43 @@ vetor.remove("b")
 
 #Remover elemento ppor indice(posição)
 del vetor[2]
+#print(vetor)
+
+#Atualizar elementos
+#atribui um novo valor para a posição especíica
+vetor[0] = "JLR"
+print(vetor)
+
+#len(IMPORTANTE)
+tamanho_vetor = len(vetor)
+print(tamanho_vetor)
+
+#ordenação
+matriculas = ["Reenam", "Gabriel", "Thais", "Breno"]
+matriculas.sort()
+print(matriculas)
+
+#cria nova lista ordenada
+novo_vetor = sorted(matriculas)
+print(novo_vetor)
+
+#Iteração : percorre os elementos
+for estudante in matriculas:
+    print(estudante)
+    
+#Dividir strings em palavras
+frase = "Pyhon é muito bom!"
+palavras = frase.split()
+print(palavras)
+
+#Juntar palavras em strings
+nova_frase = " ".join(palavras)
+print(nova_frase)
+
+#Operaçõe matemáticas em vetores numericos
+vetor_numerico = [1,2,3,4]
+for i in range(len(vetor_numerico)):
+    vetor_numerico[i] *=2 #vetor_numerico[i] = vetor_numerico[2] *2
+print(vetor_numerico)
+
+
