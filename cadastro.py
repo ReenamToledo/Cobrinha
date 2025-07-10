3dac11ef54afabd4b871fb45d6483d6fec942f4b
Reenam Toledo Fonseca <reenamtoledo364@gmail.com>
2025-07-10 16:39:41 -0300

    Add files via upload

--- cadastro.py
+++ cadastro.py
@@ -1,8 +1,23 @@
-print("1. Sim, 2. Não")
-opcao = int(input("Você quer continuar cadastrando?"))
+funcionarios = []
 
-while opcao == 1:
-    num = int(input("Digite o número: " ))
-    print(num)
-    opcao = int(input("Você quer continuar cadastrando?"))
-print("Programa encerrado!")
+while True:
+    nome = input("Digite o nome do funcionário (ou 0 para encerrar): ")
+    
+    if nome == "0":
+        break #parada
+
+
+    #impedir que o usuário envie um campo vazio de funcionarios
+    #strip remove espaços
+    elif nome.strip() == "":
+        print("Por favor insira um nome.")
+        continue
+
+    funcionarios.append(nome)
+
+#\n é uma quebra de linha
+print("\nLista de funcionários cadastrados: ")
+for i, funcionario in enumerate(funcionarios, 1): #enumerate: itera sobre a sequência
+#iterar = repetir
+#enumerate(objeto a ser percorrido, valor inicial do indice)
+    print(f"{i}. {funcionario}")
