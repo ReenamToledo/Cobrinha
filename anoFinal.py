36f6de71b340cf8ecb1f46d25c669451c958b0ed
Reenam Toledo Fonseca <reenamtoledo364@gmail.com>
2025-08-07 14:10:47 -0300

    Delete anoFinal.py

--- anoFinal.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-nome = input("Por favor me diga qual é o seu nome: ")
-idade = int(input("Por favor digite a sua idade: "))
-anoAtual = datetime.now().year
-anoDeNascimento = anoAtual - idade
-
-
-print(f"Você terá 100 anos em: {anoDeNascimento + 100} ")
