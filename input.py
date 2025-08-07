7d40b4098befd32ba07e67900b15aca941b1b30e
Reenam Toledo Fonseca <reenamtoledo364@gmail.com>
2025-08-07 14:14:04 -0300

    Delete input.py

--- input.py
+++
@@ -1,5 +0,0 @@
-nome = input("Digite seu nome: ") #guardar informações como string
-idade = int(input("Digite sua idade: ")) #
-altura = float(input("Digite a sua altura: "))
-
-print(type(nome), type(idade), type(altura))
