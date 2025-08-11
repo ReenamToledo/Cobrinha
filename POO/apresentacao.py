c8209f48015ed7d8f2738a01bf0ed6b99536a37e
Reenam Toledo Fonseca <reenamtoledo364@gmail.com>
2025-08-11 13:53:02 -0300

    Add files via upload

--- POO/apresentacao.py
+++ POO/apresentacao.py
@@ -1,14 +1,14 @@
 #Definir a classe Pessoa
 
 class Pessoa:
-    def __init__(self, nome, idade): #construtor: inicializa atributos ao criar objeto
-        self.nome = nome #atributo nome #instancia da classe = o objeto que está executando o método
+    def __init__ (self, nome, idade): #construtor: inicializa atributos ao criar o objeto
+        self.nome = nome #atributo nome  #instância atual da classe = o objeto que está executando o método
         self.idade = idade #atributo idade
-        
+    
     def apresentar(self):
         print(f"Olá, meu nome é {self.nome} e tenho {self.idade} anos.")
-        
-#Criand objeos da classe Pessoa
+    
+#Criando objetos da classe Pessoa
 pessoa1 = Pessoa("Ana", 25)
 pessoa2 = Pessoa("Thais", 27)
 pessoa3 = Pessoa("Fernando", 18)
